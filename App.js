78d35ec72e4ce9236bcda622b7cfa0a268739aa2
abmrifat1 <rifatabm1@gmail.com>
2019-10-10 20:26:26 +0600

    2nd commit

--- App.js
+++ App.js
@@ -6,54 +6,77 @@
  * @flow
  */
 
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   View,
   Text,
-  StatusBar,TextInput,TouchableOpacity
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
-
-
-const App=() => {
-  const [name,setName]=useState("");
-  const saveUserName=(name)=>{
+const App = () => {
+  const [name, setName] = useState('');
+  const saveUserName = name => {
     setName(name);
-  }
-  const [phoneNumber,setPhone]=useState("");
-  const[phonebooks,setPhonebooks]=useState([]);
-  const [namebook,setFullName]=useState([]);
-  const savePhone=(phone)=>{
-     setPhone(phone)
-  }
+  };
+  const [phoneNumber, setPhone] = useState('');
+  const [phonebooks, setPhonebooks] = useState([]);
+  const [namebook, setFullName] = useState([]);
+  const savePhone = phone => {
+    setPhone(phone);
+  };
 
-  const handleSaveBtnOnClick=()=>{
-    const fullName={
-      name
-    }
-    setFullName(currentNamebook=>[...currentNamebook,fullName])
-    console.log(namebook);
-
-
-    const phone={
-      name,phoneNumber
-    }
-    setPhonebooks(currentPhonebooks=>[...currentPhonebooks,phone])
-    console.log(phonebooks)
-  }
+  const handleSaveBtnOnClick = () => {
+    const phone = {
+      name,
+      phoneNumber,
+    };
+    setPhonebooks(currentPhonebooks => [...currentPhonebooks, phone]);
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={{padding:10,alignItems:"center"}}>
-          <TextInput placeholder="Enter your name" style={styles.inputStyle} value={name}onChangeText={saveUserName}/>
-          <TextInput placeholder="Enter your phone number" style={styles.inputStyle} keyboardType="phone-pad" value={phoneNumber} onChangeText={savePhone}/>
-          <TouchableOpacity style={{backgroundColor:"#9ee0e0",alignItems:"center",padding:10,width:"50%"}} onPress={handleSaveBtnOnClick}>
+      <SafeAreaView style={{flex:1}}>
+        <View style={{padding: 10, alignItems: 'center',flex:1}}>
+          <TextInput
+            placeholder="Enter your name"
+            style={styles.inputStyle}
+            value={name}
+            onChangeText={saveUserName}
+          />
+          <TextInput
+            placeholder="Enter your phone number"
+            style={styles.inputStyle}
+            keyboardType="phone-pad"
+            value={phoneNumber}
+            onChangeText={savePhone}
+          />
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#9ee0e0',
+              alignItems: 'center',
+              padding: 10,
+              width: '50%',
+            }}
+            onPress={handleSaveBtnOnClick}>
             <Text>Submit</Text>
           </TouchableOpacity>
-
+          <View>
+            <Text>{name}</Text><Text>{phoneNumber}</Text>
+          </View>
+          <ScrollView >
+            {phonebooks.map((record, index) => (
+              <Text key={index.toString()}>
+              Name :{record.name} , Phone :{record.phoneNumber}
+            </Text>
+            ))}
+          </ScrollView>
+         
+          
         </View>
       </SafeAreaView>
     </>
@@ -61,7 +84,12 @@ const App=() => {
 };
 
 const styles = StyleSheet.create({
-  inputStyle:{backgroundColor:"#9fffe0",color:"black",marginVertical:10,width:"100%"}
+  inputStyle: {
+    backgroundColor: '#9fffe0',
+    color: 'black',
+    marginVertical: 10,
+    width: '100%',
+  },
 });
 
 export default App;
